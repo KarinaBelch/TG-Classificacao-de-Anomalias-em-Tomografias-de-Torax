8c2de97bb66181a0d162375461e680c28fc6f197
KarinaBelch <73723978+KarinaBelch@users.noreply.github.com>
2025-08-07 12:52:11 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,15 @@
 import streamlit as st
 
-st.title("TG")
+# @title Layout
+
+# # Titulo da página
+st.set_page_config(page_title='Trabalho de Graduação', page_icon='🥼', layout='wide')
+st.title('dentificação e localização de anomalias causadas por câncer de pulmão, em tomografias de tórax, utilizando inteligência artifical')
+st.info('Trabalho de Graduação referente ao curso de Engenharia Biomédica da Universidade Federal do ABC.')
+# 
+# Menu Lateral
+st.sidebar.header("Menu")
+st.sidebar.caption("Leitura de arquivos DICOM.")
+# 
+# # Upload do arquivo
+uploaded_zip = st.file_uploader(label='Upload your DICOM file:', type="zip")
