75435dfc02ee3ebfd1d2c2b2fa04457b872ffb17
KarinaBelch <73723978+KarinaBelch@users.noreply.github.com>
2025-08-09 20:21:06 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -168,7 +168,7 @@ if uploaded_zip:
     with col2:
       flex_dados = st.container(border=True)
 
-      st.write("Arquivos DICOM encontrados:", len(dicom_files))
+      flex_dados.write(f'Arquivos DICOM encontrados: {len(dicom_files)')
       st.write("Slices detectados com câncer:", len(num_cancer))
       st.write("Slices detectados como saudável:", len(num_saudavel))
 
